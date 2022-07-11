56cb1b3abe91141f32d92018213b816d3f39a770
pooyali93 <106811535+pooyali93@users.noreply.github.com>
2022-07-11 10:44:04 +0100

    Update MyModules.js

--- src/components/pages/MyModules.js
+++ src/components/pages/MyModules.js
@@ -2,26 +2,18 @@ import ModuleItem from "../UI/ModuleItem";
 import ModuleList from "../UI/ModuleList";
 import modules from "../../data/modules";
 
-
 function MyModules() {
     // Properties ---------
-    //  Hooks ---------
+    // Hooks ---------
     // Contect ---------
     // Methods ---------
     // View ---------
     return (
         <section>
-        <h1>My Modules</h1>
-          <ModuleList modules={modules} />
-
-
-        </section>
-
-
-        
-        
+            <h1>My Modules</h1>
+            <ModuleList modules={modules} />
+        </section>   
     )
-
 }
 
 export default MyModules;
